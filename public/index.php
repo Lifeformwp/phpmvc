f61e54342088218df204feaa30fcbd2644415c2b
Serhii Kondratiuk <vielon.indie@yandex.ru>
2017-07-03 00:09:19 +0300

    phpmvc-0.2-creating-routing-configuration-basics

--- public/index.php
+++ public/index.php
@@ -1,3 +1,29 @@
 <?php
 
-echo 'Requested URL = "' . $_SERVER['QUERY_STRING'] . '"';
+/**
+ * Front controller
+ */
+
+require '../Core/Router.php';
+
+$router = new Router();
+
+//echo get_class($router);
+
+$router->add('', ['controller' => 'Home', 'action' => 'index']);
+$router->add('posts', ['controller' => 'Post', 'action' => 'index']);
+$router->add('posts/new', ['controller' => 'Posts', 'action' => 'new']);
+
+//echo '<pre>';
+//var_dump($router->getRoutes());
+//echo '</pre>';
+
+$url = $_SERVER['QUERY_STRING'];
+
+if ($router->match($url)) {
+    echo '<pre>';
+    var_dump($router->getParams());
+    echo '</pre>';
+} else {
+    echo "No route found for URL '$url'";
+}
