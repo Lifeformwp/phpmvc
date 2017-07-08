0c4a052b925e2649960c36102e20833862e1bd9c
Serhii Kondratiuk <vielon.indie@yandex.ru>
2017-07-08 13:44:35 +0300

    phpmvc-0.4-configured-routing-for-ids

--- Core/Router.php
+++ Core/Router.php
@@ -9,8 +9,12 @@ class Router
 
     protected $params = [];
 
-    public function add($route, $params)
+    public function add($route, $params = [])
     {
+        $route = preg_replace('/\//', '\\/', $route);
+        $route = preg_replace('/\{([a-z]+)\}/', '(?P<\1>[a-z-]+)', $route);
+        $route = preg_replace('/\{([a-z]+):([^\}]+)\}/', '(?P<\1>\2)', $route);
+        $route = '/^' . $route . '$/i';
         $this->routes[$route] = $params;
     }
 
@@ -21,25 +25,22 @@ class Router
 
     public function match($url)
     {
-//        foreach ($this->routes as $route => $params) {
-//            if ($url == $route) {
-//                $this->params = $params;
-//                return true;
-//            }
-//        }
-        $reg_exp = "/^(?P<controller>[a-z]+)\/(?P<action>[a-z]+)$/";
+        //$reg_exp = "/^(?P<controller>[a-z]+)\/(?P<action>[a-z]+)$/";
 
-        if (preg_match($reg_exp, $url, $matches)) {
-            $params = [];
-            foreach ($matches as $key => $match) {
-                if (is_string($key)) {
-                    $params[$key] = $match;
+        foreach ($this->routes as $route => $params) {
+            if (preg_match($route, $url, $matches)) {
+                //$params = [];
+                foreach ($matches as $key => $match) {
+                    if (is_string($key)) {
+                        $params[$key] = $match;
+                    }
                 }
-            }
 
-            $this->params = $params;
-            return true;
+                $this->params = $params;
+                return true;
+            }
         }
+        return false;
     }
 
     public function getParams()
--- public/index.php
+++ public/index.php
@@ -12,11 +12,14 @@ $router = new Router();
 
 $router->add('', ['controller' => 'Home', 'action' => 'index']);
 $router->add('posts', ['controller' => 'Post', 'action' => 'index']);
-$router->add('posts/new', ['controller' => 'Posts', 'action' => 'new']);
+//$router->add('posts/new', ['controller' => 'Posts', 'action' => 'new']);
+$router->add('{controller}/{action}');
+$router->add('{controller}/{id:\d+}/{action}');
 
-//echo '<pre>';
+echo '<pre>';
 //var_dump($router->getRoutes());
-//echo '</pre>';
+echo htmlspecialchars(print_r($router->getRoutes(), true));
+echo '</pre>';
 
 $url = $_SERVER['QUERY_STRING'];
 
